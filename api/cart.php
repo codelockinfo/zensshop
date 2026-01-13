75f4e367ba3b168c8339f51bdd0135085535c471
codelock <codelock2021@gmail.com>
2026-01-13 16:06:16 +0530

    Deploy

--- api/cart.php
+++ api/cart.php
@@ -4,10 +4,10 @@
  * Handles cart operations via AJAX
  */
 
-header('Content-Type: application/json');
 require_once __DIR__ . '/../classes/Cart.php';
 
 $cart = new Cart();
+header('Content-Type: application/json');
 $method = $_SERVER['REQUEST_METHOD'];
 $input = json_decode(file_get_contents('php://input'), true);
 
